ce0c3760973f7ee52346f7ea5cbaa14a6d7ba773
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2021-09-05 23:01:58 +0200

    In memory dataloader

--- model/dataloader.py
+++ model/dataloader.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os
 import glob
 import numpy as np
+from tqdm import tqdm
 #import matplotlib
 
 
@@ -23,25 +24,37 @@ class InfektaDataset(Dataset):
         print(self.sample_per_folder)
         self.transform = transform
         self.target_transform = target_transform
+        self.data = []
+        for folder in tqdm(self.folders):
+            fdata = []
+            for i in range(len(glob.glob(self.folders[0]+"/*"))):
+                img_path = os.path.join(folder, str(i)+".npy")
+                fdata += [np.load(img_path)*self.people_factor]
+            self.data += [np.vstack(fdata)]
+
+
 
     def __len__(self):
         return self.folders_count*self.sample_per_folder
     def __getitem__(self, idxn):
         idx = idxn % self.sample_per_folder
         idc = int(idxn / self.sample_per_folder)
-        imgs = []
-        for  i in range (self.time_frames_count):
-            img_path = os.path.join(self.folders[idc], str(idx+i)+".npy")
-            image = np.load(img_path)*self.people_factor
-            imgs +=[image]
-        img_path = os.path.join(self.folders[idc], str(idx+self.time_frames_count)+".npy")
-        target = np.load(img_path)*self.people_factor
+        imgs = self.data[idc][idx*8:(idx+self.time_frames_count)*8]
+        # for  i in range (self.time_frames_count):
+        #     img_path = os.path.join(self.folders[idc], str(idx+i)+".npy")
+        #     image = np.load(img_path)*self.people_factor
+        #     imgs +=[image]
+        # img_path = os.path.join(self.folders[idc], str(idx+self.time_frames_count)+".npy")
+        # target = np.load(img_path)*self.people_factor
+        target = self.data[idc][(idx+self.time_frames_count)*8:(idx+self.time_frames_count+1)*8]
         if self.transform:
-            image = self.transform(image)
+            image = self.transform(imgs)
         if self.target_transform:
             target = self.target_transform(target)
-        return np.vstack(imgs), target
+        return imgs, target
 if __name__ =="__main__":
-    infekta = InfektaDataset("../INFEKTA-HD/dump/")
-    print(infekta[0])
+    infekta = InfektaDataset("../INFEKTA-HD/data/runs16/",2)
+    print(infekta[0][0].shape)
+    print(infekta[0][1].shape)
+
     #matplotlib.image.imsave(str(0).zfill(5) + ".png", infekta[7][0][3])
